e0f198b83480bafb16cd2957f7dcfbacb2b2acfb
netking1228 <134265364+netking1228@users.noreply.github.com>
2024-11-15 15:02:25 +0800

    111

---
+++ assets/js/share.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let videoSrc = "https://www.youtube.com/embed/0TZs6--ssFM?si=6bT100jPDmqAUXmn&autoplay=1&mute=1";
+    document.getElementById("myIframe").src = videoSrc;
+});
